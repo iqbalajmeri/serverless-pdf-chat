4fe27ca9d4ed12b1ec15da1e0ee0f4844f971bf3
jyotirmayee23 <jyotirmayee.parida@operisoft.com>
2024-03-05 17:38:00 +0530

    frontend changes

--- frontend/src/components/DocumentDetail.tsx
+++ frontend/src/components/DocumentDetail.tsx
@@ -1,6 +1,10 @@
 import { Document } from "../common/types";
 import { getDateTime } from "../common/utilities";
 import { filesize } from "filesize";
+import { BsThreeDots } from "react-icons/bs";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { API } from "aws-amplify";
 import {
   DocumentIcon,
   CircleStackIcon,
@@ -9,18 +13,84 @@ import {
   CloudIcon,
   CogIcon,
 } from "@heroicons/react/24/outline";
+import { useEffect } from "react";
+ 
 
-const DocumentDetail: React.FC<Document> = (document: Document) => {
+ 
+const DocumentDetail: React.FC<Document & { handleDeletFull: (documentId: string, conversationIds: string[]) => void, handleViewFile: (url: string) => void }> = ({ handleDeletFull, handleViewFile, ...document }) => {
+  // console.log("document", document)
+ 
+  useEffect(() => {
+  }, [document])
+ 
+  // const handleDeletFull = async (documentId, conversationIds) => {
+  //   // console.log(Array.isArray(conversationIds))
+  //   // console.log("document id", documentId)
+  //   // console.log("conversation ids", conversationIds)
+  //   alert("Do you want to delete this file?")
+  //   const deleteFull = async (documentId, conversationIds) => {
+  //     try {
+  //       const response = await API.del(
+  //         'serverless-pdf-chat',
+  //         '/Delete_Full',
+  //         {
+  //           body: {
+  //             document_id: documentId,
+  //             conversation_ids: conversationIds,
+  //           },
+  //         }
+  //       );
+  //       console.log('Delete request successful', response);
+  //     } catch (error) {
+  //       console.error('Error during delete request:', error);
+  //     }
+  //   };
+ 
+  //   deleteFull(documentId, conversationIds);
+  // }
+ 
   return (
     <>
+      <div className="flex justify-end">
+        <Popup
+          trigger={<div className="menu-item"> <p><BsThreeDots /></p> </div>}
+          position="right top"
+          on="hover"
+          closeOnDocumentClick
+          mouseLeaveDelay={300}
+          mouseEnterDelay={0}
+          contentStyle={{ padding: '0px', border: 'none' }}
+          arrow={false}
+        >
+          <div className="menu">
+            <div className="menu-item py-1 px-2">
+              <p
+                className="text-sm"
+                onClick={() => handleDeletFull(document.documentid, document.conversations.map(conv => conv.conversationid))}>
+                Delete File
+              </p>
+            </div>
+            {document.s3_object_url && (
+              <div className="menu-item py-1 px-2">
+                <p
+                  className="text-sm"
+                  onClick={() => handleViewFile(document.s3_object_url)}
+                >
+                  View File
+                </p>
+              </div>
+            )}
+          </div>
+        </Popup>
+      </div>
       <h3 className="text-center mb-3 text-lg font-bold tracking-tight text-gray-900">
         {document.filename}
       </h3>
       <div className="flex flex-col space-y-2">
-        <div className="inline-flex items-center">
+        {/* <div className="inline-flex items-center">
           <DocumentIcon className="w-4 h-4 mr-2" />
           {document.pages} pages
-        </div>
+        </div> */}
         <div className="inline-flex items-center">
           <CircleStackIcon className="w-4 h-4 mr-2" />
           {filesize(Number(document.filesize)).toString()}
@@ -57,5 +127,5 @@ const DocumentDetail: React.FC<Document> = (document: Document) => {
     </>
   );
 };
-
+ 
 export default DocumentDetail;
